1b555886894f89fdbb24f6a61b96ff2c3c6827e5
AR123456 <coloradoz99@gmail.com>
2023-09-26 06:05:50 -0600

    adding some fade to showcase with toggle

--- Hamburger With Overlay/app.js
+++ Hamburger With Overlay/app.js
@@ -1,2 +1,17 @@
 //  this code works without javascript because on the use of input type of check box and class of toggler
 //  styling the checked state
+// Get the checkbox element
+const toggler = document.querySelector(".toggler");
+
+// Get the container element
+const showcase = document.querySelector(".showcase");
+
+// Add an event listener to the checkbox
+toggler.addEventListener("change", function () {
+  console.log("checked ");
+  if (this.checked) {
+    showcase.classList.add("checked");
+  } else {
+    showcase.classList.remove("checked");
+  }
+});
